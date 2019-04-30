99e9fa6192f3339f13160a4b0db1d637d1c4ba97
Gilberto Costa Junior <gil-cos@hotmail.com>
2019-04-30 10:00:39 -0300

    radarsjc-ui initial commit

---
+++ radarsjc-ui/src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  exports: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class CoreModule { }
