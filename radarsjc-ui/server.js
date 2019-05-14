6f277f9ac6a4960c7a5f3f58024ac0683eec5f79
Gilberto Costa Junior <gil-cos@hotmail.com>
2019-05-14 11:35:29 -0300

    heroku

---
+++ radarsjc-ui/server.js
@@ -0,0 +1,17 @@
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/radarsjc-ui'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/radarsjc-ui/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
